396add3673852d26754e70068ce73cb0d8bd39ba
luci-gutoiu <lucian.gutoiu@yahoo.com>
2020-05-13 10:00:22 +0000

    Updates

---
+++ precache-manifest.00efcf919001ebb76b8b1715ed45e4dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e7551d3b564a3d727e50ca74141b1f7e",
+    "url": "/learn-react/index.html"
+  },
+  {
+    "revision": "a20aa1deead05805dbc0",
+    "url": "/learn-react/static/css/main.f97341ff.chunk.css"
+  },
+  {
+    "revision": "24211de46fa150991bd1",
+    "url": "/learn-react/static/js/2.b6430e1b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-react/static/js/2.b6430e1b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a20aa1deead05805dbc0",
+    "url": "/learn-react/static/js/main.3fe38fdb.chunk.js"
+  },
+  {
+    "revision": "c82a5b59c5412b88b791",
+    "url": "/learn-react/static/js/runtime-main.6d07fe9c.js"
+  }
+]);
